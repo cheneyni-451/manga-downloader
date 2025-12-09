005bcf08534dbe51b42dffd53aaa77387bf24d77
Cheney Ni <56236030+cheneyni-451@users.noreply.github.com>
2025-12-08 23:29:20 -0500

    add multithreading and progress bar

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ use std::{
 };
 
 use clap::Parser;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use reqwest::{
     blocking::Client,
     header::{self, HeaderMap, HeaderValue},
 };
 use scraper::Selector;
 
-fn download_file(client: Client, url: &str, file_path: PathBuf) -> Result<(), Box<dyn Error>> {
+fn download_file(client: &Client, url: &str, file_path: PathBuf) -> Result<(), Box<dyn Error>> {
     let response = client.get(url).send()?;
     let content = response.bytes()?;
 
@@ -23,7 +24,13 @@ fn download_file(client: Client, url: &str, file_path: PathBuf) -> Result<(), Bo
     Ok(())
 }
 
-fn download_chapter(client: Client, chapter_url: &str, chapter_title: &str, args: Args) {
+fn download_chapter(
+    client: &Client,
+    chapter_url: &str,
+    chapter_title: &str,
+    args: &Args,
+    progress_bar: &ProgressBar,
+) {
     let response = client.get(chapter_url).send();
     if let Ok(result) = response {
         let html_content = result.text().unwrap_or_default();
@@ -37,14 +44,15 @@ fn download_chapter(client: Client, chapter_url: &str, chapter_title: &str, args
             .for_each(|(page_num, image)| {
                 let page_url = image
                     .attr("src")
-                    .unwrap_or(image.attr("data-src").unwrap_or_default());
+                    .unwrap_or_else(|| image.attr("data-src").unwrap_or_default());
 
                 let page_path = PathBuf::from(format!(
                     "tmp/{title}/{chapter_title}/{page_num:03}.jpg",
                     title = args.title
                 ));
 
-                download_file(client.clone(), page_url, page_path).unwrap();
+                download_file(client, page_url, page_path).unwrap();
+                progress_bar.tick();
             });
     }
 }
@@ -55,7 +63,7 @@ struct Chapter {
     title: String,
 }
 
-fn get_num_chapter_urls(client: Client, title_url: &str) -> Vec<Chapter> {
+fn get_num_chapter_urls(client: &Client, title_url: &str) -> Vec<Chapter> {
     let response = client.get(title_url).send();
     if let Ok(result) = response {
         let html_content = result.text().unwrap_or_default();
@@ -78,6 +86,46 @@ fn get_num_chapter_urls(client: Client, title_url: &str) -> Vec<Chapter> {
     }
 }
 
+fn split_jobs<T: Clone>(jobs: &mut Vec<T>, num_batches: usize) -> Vec<Vec<T>> {
+    let n = jobs.len();
+
+    let chunk_size = n / num_batches;
+
+    let mut batches = Vec::with_capacity(num_batches);
+    for _ in 0..(n % num_batches) {
+        let batch = jobs.split_off(jobs.len() - (chunk_size + 1));
+        batches.push(batch);
+    }
+    batches.extend(jobs.chunks_exact(chunk_size).map(|c| c.to_owned()));
+
+    batches
+}
+
+fn download_chapters(
+    client: &Client,
+    args: &Args,
+    chapters: &Vec<Chapter>,
+    chapter_progress: &ProgressBar,
+    total_progress: &ProgressBar,
+) {
+    chapter_progress.tick();
+    for Chapter {
+        url,
+        title: chapter_title,
+    } in chapters
+    {
+        chapter_progress.set_message(format!("downloading {chapter_title}"));
+
+        let chapter_url = format!("{HOST_URL}{url}");
+        download_chapter(client, &chapter_url, chapter_title, args, chapter_progress);
+
+        chapter_progress.inc(1);
+        total_progress.inc(1);
+    }
+
+    chapter_progress.finish_and_clear();
+}
+
 #[derive(Parser, Debug, Clone)]
 struct Args {
     #[arg(short, long)]
@@ -90,14 +138,13 @@ struct Args {
     threads: usize,
 }
 
+const HOST_URL: &str = "https://mangapill.com";
+
 fn main() {
     let args = Args::parse();
 
     let mut headers = HeaderMap::new();
-    headers.insert(
-        header::REFERER,
-        HeaderValue::from_static("https://mangapill.com/"),
-    );
+    headers.insert(header::REFERER, HeaderValue::from_static(HOST_URL));
 
     let client = Client::builder()
         .default_headers(headers)
@@ -105,37 +152,56 @@ fn main() {
         .build()
         .unwrap();
 
-    let title_url = format!(
-        "https://mangapill.com/manga/{id}/{title}",
-        id = args.id,
-        title = args.title
+    let mut chapters = get_num_chapter_urls(
+        &client,
+        &format!(
+            "{HOST_URL}/manga/{id}/{title}",
+            id = args.id,
+            title = args.title
+        ),
     );
-    let chapters = get_num_chapter_urls(client.clone(), &title_url);
 
+    let book_path = format!("tmp/{}", args.title);
+
+    let num_chapters = chapters.len();
     chapters.iter().for_each(|Chapter { url: _, title }| {
-        fs::create_dir_all(format!("tmp/{book_title}/{title}", book_title = args.title)).unwrap();
+        fs::create_dir_all(format!("{book_path}/{title}")).unwrap();
     });
 
-    let mut handles = vec![];
-    chapters
-        .chunks(chapters.len() / args.clone().threads)
-        .map(|c| c.to_owned())
-        .for_each(|chunk| {
-            let args = args.clone();
-            let client = client.clone();
-            handles.push(thread::spawn(move || {
-                let chunk_size = chunk.len();
-                chunk
-                    .iter()
-                    .enumerate()
-                    .for_each(|(i, Chapter { url, title })| {
-                        let chapter_url = format!("https://mangapill.com{url}");
-                        // println!("{chapter_url}");
-                        download_chapter(client.clone(), &chapter_url, title, args.clone());
-                        println!("downloaded {title:<13} {}/{}", i + 1, chunk_size);
-                    });
-            }))
-        });
+    let num_threads = args.threads.min(num_chapters);
+    let batches = split_jobs(&mut chapters, num_threads);
 
-    handles.into_iter().for_each(|h| h.join().unwrap());
+    let multi_progress = MultiProgress::new();
+    let multi_progress_style = ProgressStyle::with_template(
+        "{spinner:.yellow} [{bar:60.yellow/white}] {pos:>4}/{len} chaps - {msg}",
+    )
+    .unwrap()
+    .progress_chars("-Cco");
+    let total_progress = multi_progress.add(ProgressBar::new(num_chapters.try_into().unwrap()));
+    total_progress.set_style(
+        ProgressStyle::with_template("  [{bar:60.green/blue}] {pos:>4}/{len} chaps")
+            .unwrap()
+            .progress_chars("▓▒░"),
+    );
+    total_progress.tick();
+
+    let mut threads = vec![];
+
+    for batch in batches {
+        let args = args.clone();
+        let client = client.clone();
+
+        let chapter_progress = ProgressBar::new(batch.len().try_into().unwrap());
+        chapter_progress.set_style(multi_progress_style.clone());
+        let chapter_progress = multi_progress.insert_before(&total_progress, chapter_progress);
+
+        let total_progress = total_progress.clone();
+
+        threads.push(thread::spawn(move || {
+            download_chapters(&client, &args, &batch, &chapter_progress, &total_progress)
+        }));
+    }
+
+    threads.into_iter().for_each(|h| h.join().unwrap());
+    total_progress.finish_with_message(format!("downloaded {num_chapters} chapters to "));
 }
