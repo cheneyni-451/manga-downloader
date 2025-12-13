92d461f0541b4c9825ee375834a8be158b4350a4
Cheney Ni <56236030+cheneyni-451@users.noreply.github.com>
2025-12-13 17:47:35 -0500

    refactor main fn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use std::{
-    error::Error,
-    fmt::Display,
-    fs::{self, File},
-    io::Write,
-    path::{Path, PathBuf},
-    process::exit,
-    thread::{self},
-};
+use std::{error::Error, fmt::Display, fs, io::Write, path::Path, thread};
 
 use clap::Parser;
 use dialoguer::{Select, console::Style, theme::ColorfulTheme};
@@ -22,7 +14,7 @@ fn download_file(client: &Client, url: &str, file_path: &Path) -> Result<(), Box
     let response = client.get(url).send()?;
     let content = response.bytes()?;
 
-    let mut downloaded_file = File::create(file_path)?;
+    let mut downloaded_file = fs::File::create(file_path)?;
     downloaded_file.write_all(&content)?;
 
     Ok(())
@@ -31,7 +23,7 @@ fn download_file(client: &Client, url: &str, file_path: &Path) -> Result<(), Box
 fn download_chapter(
     client: &Client,
     chapter_url: &str,
-    chapter_dst: &Path,
+    chapter_path: &Path,
     progress_bar: &ProgressBar,
 ) {
     let response = client.get(chapter_url).send();
@@ -49,7 +41,7 @@ fn download_chapter(
                     .attr("src")
                     .unwrap_or_else(|| image.attr("data-src").unwrap_or_default());
 
-                let page_path = chapter_dst.join(format!("{page_num:03}.jpg"));
+                let page_path = chapter_path.join(format!("{page_num:03}.jpg"));
 
                 download_file(client, page_url, &page_path).unwrap();
                 progress_bar.tick();
@@ -70,26 +62,28 @@ impl Display for Chapter {
 }
 
 fn fetch_chapters_urls(client: &Client, title_url: &str) -> Vec<Chapter> {
-    let response = client.get(title_url).send();
-    if let Ok(result) = response {
-        let html_content = result.text().unwrap_or_default();
-        let doc = scraper::Html::parse_document(&html_content);
+    match client.get(title_url).send() {
+        Ok(response) => {
+            let html_content = response.text().unwrap_or_default();
+            let doc = scraper::Html::parse_document(&html_content);
 
-        let selector = Selector::parse("#chapters a").unwrap();
-        doc.select(&selector)
-            .map(|a| Chapter {
-                url: a.attr("href").unwrap_or_default().to_string(),
-                title: a
-                    .attr("title")
-                    .unwrap_or_default()
-                    .trim()
-                    .to_ascii_lowercase()
-                    .to_string(),
-            })
-            .rev()
-            .collect()
-    } else {
-        vec![]
+            let selector = Selector::parse("#chapters a").unwrap();
+            doc.select(&selector)
+                .map(|a| Chapter {
+                    url: a.attr("href").unwrap_or_default().to_string(),
+                    title: a
+                        .attr("title")
+                        .unwrap_or_default()
+                        .trim()
+                        .to_ascii_lowercase()
+                        .to_string(),
+                })
+                .rev()
+                .collect()
+        }
+        Err(_) => {
+            vec![]
+        }
     }
 }
 
@@ -103,7 +97,9 @@ fn split_jobs<T: Clone>(jobs: &mut Vec<T>, num_batches: usize) -> Vec<Vec<T>> {
         let batch = jobs.split_off(jobs.len() - (chunk_size + 1));
         batches.push(batch);
     }
-    batches.extend(jobs.chunks_exact(chunk_size).map(|c| c.to_owned()));
+    while !jobs.is_empty() {
+        batches.push(jobs.split_off(jobs.len().saturating_sub(chunk_size)));
+    }
 
     batches
 }
@@ -123,10 +119,34 @@ fn get_title_from_id(client: &Client, id: usize) -> Option<(String, Url)> {
     None
 }
 
+fn select_chapters(mut chapters: Vec<Chapter>) -> Vec<Chapter> {
+    let selection_theme = ColorfulTheme {
+        prompt_style: Style::default().blue(),
+        active_item_style: Style::default().reverse(),
+        ..Default::default()
+    };
+    let chapter_selection_start = Select::with_theme(&selection_theme)
+        .with_prompt("First of the chapters to download")
+        .items(&chapters)
+        .max_length(10)
+        .interact()
+        .unwrap();
+    chapters.drain(0..chapter_selection_start);
+    let chapter_selection_end = Select::with_theme(&selection_theme)
+        .with_prompt("Last of the chapters to download")
+        .items(&chapters)
+        .max_length(10)
+        .interact()
+        .unwrap();
+    chapters.truncate(chapter_selection_end + 1);
+
+    chapters
+}
+
 fn download_chapters(
     client: &Client,
     chapters: &Vec<Chapter>,
-    manga_dst: &Path,
+    manga_path: &Path,
     chapter_progress: &ProgressBar,
     total_progress: &ProgressBar,
 ) {
@@ -142,7 +162,7 @@ fn download_chapters(
         download_chapter(
             client,
             &chapter_url,
-            &manga_dst.join(chapter_title),
+            &manga_path.join(chapter_title),
             chapter_progress,
         );
 
@@ -179,51 +199,30 @@ fn main() {
         .build()
         .unwrap();
 
-    let maybe_title = get_title_from_id(&client, args.id);
-    if maybe_title.is_none() {
-        println!("Invalid manga id");
-        exit(1);
-    }
-    let (title, title_url) = maybe_title.unwrap();
-
-    let mut chapters = fetch_chapters_urls(&client, title_url.as_ref());
-
-    let selection_theme = ColorfulTheme {
-        prompt_style: Style::default().blue(),
-        active_item_style: Style::default().reverse(),
-        ..Default::default()
-    };
-    let chapter_selection_start = Select::with_theme(&selection_theme)
-        .with_prompt("First of the chapters to download")
-        .items(&chapters)
-        .max_length(10)
-        .interact()
-        .unwrap();
-    chapters.drain(0..chapter_selection_start);
-    let chapter_selection_end = Select::with_theme(&selection_theme)
-        .with_prompt("Last of the chapters to download")
-        .items(&chapters)
-        .max_length(10)
-        .interact()
-        .unwrap();
-    chapters.truncate(chapter_selection_end + 1);
-
-    let book_path = PathBuf::from("tmp").join(title);
-
-    let num_chapters = chapters.len();
-    chapters.iter().for_each(|Chapter { url: _, title }| {
-        fs::create_dir_all(book_path.join(title)).unwrap();
+    let (title, title_url) = get_title_from_id(&client, args.id).unwrap_or_else(|| {
+        eprintln!("Invalid manga id: {}", args.id);
+        std::process::exit(1);
     });
 
+    let all_chapters = fetch_chapters_urls(&client, title_url.as_ref());
+    let mut selected_chapters = select_chapters(all_chapters);
+
+    let book_path = Path::new("tmp").join(title);
+
+    let num_chapters = selected_chapters.len();
+    for Chapter { title, .. } in &selected_chapters {
+        fs::create_dir_all(book_path.join(title)).unwrap();
+    }
+
     let num_threads = args.threads.min(num_chapters);
-    let batches = split_jobs(&mut chapters, num_threads);
+    let batches = split_jobs(&mut selected_chapters, num_threads);
 
     let multi_progress = MultiProgress::new();
     let multi_progress_style = ProgressStyle::with_template(
         "{spinner:.yellow} [{bar:60.yellow/white}] {pos:>4}/{len} chaps - {msg}",
     )
     .unwrap()
-    .tick_chars("\\|/- ")
+    .tick_chars("⣦⣇⡏⠟⠻⢹⣸⣴ ")
     .progress_chars("-Cco");
     let total_progress = multi_progress.add(ProgressBar::new(num_chapters.try_into().unwrap()));
     total_progress.set_style(
